e629b1a11abf70a72e6245b0e3d2755951f50e18
Olival Paulino <olivalpaulinocc@gmail.com>
2025-11-25 11:10:57 -0300

    atualizando imagens

--- data.js
+++ data.js
@@ -2,75 +2,75 @@
 const products = [
     {
         id: 1,
-        title: "Vestido Casamento Festa Convidada Casual Elegante Premium",
-        price: 144.34,
-        image: "https://http2.mlstatic.com/D_NQ_NP_2X_909935-MLB95649921579_102025-F-vestido-casamento-festa-convidada-casual-elegante-premium.webp",
+        title: "Cadeira de Descanso e Balanço Funtime 18kgs Coruja Maxi Baby",
+        price: 189.98,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_897453-MLA96112590357_102025-F.webp",
         shipping: "Frete grátis",
         freeShipping: true,
-        link: "https://mercadolivre.com/sec/2qVhskv" // Adicione seu link aqui
+        link: "https://mercadolivre.com/sec/28h2Azr" // Adicione seu link aqui
     },
     {
         id: 2,
-        title: "Vestido Feminino Festa Casamento Convidada Casual Elegante",
-        price: 152.00,
-        image: "https://http2.mlstatic.com/D_NQ_NP_2X_742171-MLB98471770372_112025-F-vestido-feminino-festa-casamento-convidada-casual-elegante.webp",
+        title: "Kit 15 Peças Roupas Bebê Body Calça Touca Enxoval Completo",
+        price: 68.90,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_660568-MLB88359216828_072025-F-kit-15-pecas-roupas-beb-body-calca-touca-enxoval-completo.webp",
         shipping: "Frete grátis",
         freeShipping: true,
-        link: "https://mercadolivre.com/sec/1mh55GA" // Adicione seu link aqui
+        link: "https://mercadolivre.com/sec/1TxPnsK" // Adicione seu link aqui
     },
     {
         id: 3,
-        title: "Vestido Longo Babado Fenda Manga 3/4 Evangelico Elegante",
-        price: 78.99,
-        image: "https://http2.mlstatic.com/D_NQ_NP_2X_603645-MLB98066435727_112025-F-vestido-longo-babado-fenda-manga-34-evangelico-elegante.webp",
+        title: "Kit 5 Macacão De Bebê Vira Pé Estampado - Menino E Menina",
+        price: 67.19,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_758566-MLB97108805764_112025-F-kit-5-macaco-de-beb-vira-pe-estampado-menino-e-menina.webp",
         shipping: "Frete grátis",
         freeShipping: true,
-        link: "https://mercadolivre.com/sec/19pqBMT" // Adicione seu link aqui
+        link: "https://mercadolivre.com/sec/1B6HZEm" // Adicione seu link aqui
     },
     {
         id: 4,
-        title: "Vestido Femenino Midi Elegante Moda Evangelica",
-        price: 167.71,
-        image: "https://http2.mlstatic.com/D_NQ_NP_2X_808791-MLB88472042378_072025-F-vestido-femenino-midi-elegante-moda-evangelica.webp",
+        title: "Kit Bolsa Mala Bebe Saída De Maternidade Mega Promoção",
+        price: 139.99,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_730605-MLB95926174392_102025-F-kit-bolsa-mala-bebe-saida-de-maternidade-mega-promoco.webp",
         shipping: "Frete grátis",
         freeShipping: true,
-        link: "https://mercadolivre.com/sec/2diTvAF" // Adicione seu link aqui
+        link: "https://mercadolivre.com/sec/14v4kiF" // Adicione seu link aqui
     },
     {
         id: 5,
-        title: "Vestido Feminino Longo De Alcinha Decote Em V",
-        price: 120.02,
-        image: "https://http2.mlstatic.com/D_NQ_NP_2X_761351-MLB84663481439_052025-F-vestido-feminino-longo-de-alcinha-decote-em-v.webp",
+        title: "Kit De Cuidados E Higiene Do Bebê Recém Nascido 13 Peças Rosa",
+        price: 55.09,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_883106-MLB89560860671_082025-F.webp",
         shipping: "Frete grátis",
         freeShipping: true,
-        link: "https://mercadolivre.com/sec/1w2vuT3" // Adicione seu link aqui
+        link: "https://mercadolivre.com/sec/2X71phw" // Adicione seu link aqui
     },
     {
         id: 6,
-        title: "Vestido Feminino Longo Tomara Que Caia Elegante",
-        price: 87.90,
-        image: "https://http2.mlstatic.com/D_NQ_NP_2X_642169-MLB93440373574_092025-F-vestido-feminino-longo-tomara-que-caia-elegante.webp",
+        title: "Berço Bebê Moises Ajustável Mosqueteiro Portátil Infantil Rosa-claro",
+        price: 692.92,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_617396-MLB97309932192_112025-F.webp",
         shipping: "Frete grátis",
         freeShipping: true,
-        link: "https://mercadolivre.com/sec/1xaSGgR" // Adicione seu link aqui
+        link: "https://mercadolivre.com/sec/2waig2G" // Adicione seu link aqui
     },
     {
         id: 7,
-        title: "Vestido Midi Nula Manga Noiva Offwhite Laço Casamento Civil",
-        price: 172.53,
-        image: "https://http2.mlstatic.com/D_NQ_NP_2X_807800-MLB93835326228_102025-F-vestido-midi-nula-manga-noiva-offwhite-laco-casamento-civil.webp",
+        title: "Carrinho De Passeio Para Bebê Spot até 15kg Voyage IMP01766",
+        price: 399.36,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_778167-MLA96099109647_102025-F.webp",
         shipping: "Frete grátis",
         freeShipping: true,
-        link: "https://mercadolivre.com/sec/21fXg4e" // Adicione seu link aqui
+        link: "https://mercadolivre.com/sec/2e85YdZ" // Adicione seu link aqui
     },
     {
         id: 8,
-        title: "Vestido Curto Justo Tubinho Fenda Decote V Alça Larga",
-        price: 94.52,
-        image: "https://http2.mlstatic.com/D_NQ_NP_2X_948511-MLB88839759507_072025-F-vestido-curto-justo-tubinho-fenda-decote-v-alca-larga.webp",
+        title: "Kit De Cuidados De Saúde De Beleza Do Bebê Com Termômetro Cor Azul",
+        price: 33.34,
+        image: "https://http2.mlstatic.com/D_NQ_NP_2X_698803-MLU70921776831_082023-F.webp",
         shipping: "Frete grátis",
         freeShipping: true,
-        link: "https://mercadolivre.com/sec/17oTmnP" // Adicione seu link aqui
+        link: "https://mercadolivre.com/sec/2tJTRae" // Adicione seu link aqui
     }
 ];
 
